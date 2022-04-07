5051ef50527d2642bfa011c286c5d80edcf73011
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-04-07 12:17:21 +0200

    explicitely create the admin user once during the setup

--- templates/start-keycloak.sh
+++ templates/start-keycloak.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export HOME=/home/pacs/{{pac}}/users/{{user}}
 
-export KEYCLOAK_ADMIN={{admin_username}}
-export KEYCLOAK_ADMIN_PASSWORD={{admin_password}}
 export PROXY_ADDRESS_FORWARDING=true
 export KEYCLOAK_FRONTEND_URL="https://{{domain}}/auth"
 
