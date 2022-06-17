b2d62271d33943191f9e89d923d3dadeba440ee8
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-06-17 17:03:53 +0200

    fix issues with http vs https: add X-Forwarded-Proto Http Header. Keycloak service should only listen on localhost. Upgrade to latest release 18.0.1

--- templates/start-keycloak.sh
+++ templates/start-keycloak.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 export HOME=/home/pacs/{{pac}}/users/{{user}}
 
-export PROXY_ADDRESS_FORWARDING=true
-export KEYCLOAK_FRONTEND_URL="https://{{domain}}/auth"
-
+export PROXY_ADDRESS_FORWARDING="true"
 
 cd $HOME/keycloak
-./bin/kc.sh start --http-port {{keycloak_http_port}} &
+./bin/kc.sh start --http-port {{keycloak_http_port}} --http-host 127.0.0.1 --proxy edge &
 echo $! >$HOME/var/run/keycloak.pid
